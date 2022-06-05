764057fd93865601fd7a1cc5b7e37e8b25668202
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-05 12:12:30 +0900

    Update Prog92Test.java

--- src/test/java/Prog92Test.java
+++ src/test/java/Prog92Test.java
@@ -1,7 +1,10 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 import java.io.*;
-
+/**
+ * @version (20220604)
+ **/
 public class Prog92Test {
 
     @Test
@@ -18,11 +21,21 @@ public class Prog92Test {
         // in.inputln("2"); // 標準入力をテストする場合
         // Hello.main(new String[]{"1", "2", "3"}); // 実行時引数をテストする場合
         Prog92.main(null);
-
-        // assertion
-        assertEquals("Hello world!\n", bos.toString());
-
         // undo the binding in System
         System.setOut(originalOut);
+        
+        // assertion
+        String[] prints = bos.toString().split("\r\n|\n", -1);
+
+        try {
+            assertEquals("工太は攻撃した！", prints[0]);
+            assertEquals("敵に5ポイントのダメージをあたえた！", prints[1]);
+            assertEquals("勇者工太は姿を消した！", prints[2]);
+            assertEquals("工太は見えない攻撃をした！", prints[3]);
+            assertEquals("敵に10ポイントのダメージをあたえた！", prints[4]);
+        } catch (ArrayIndexOutOfBoundsException excpt) {
+            fail("Prog92.main()のprint出力が5行ではありません!");
+        }
+        assertEquals(6,prints.length,"改行数が6つ以上あります!"); // このif文に到達した時点で prints.lengthは6以上になっている
     }
 }
